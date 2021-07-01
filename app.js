7ec7f76088e817ac8e8cc35e1b559921473f7473
Dipesh Patil <dipeshpatil013@gmail.com>
2021-07-01 19:58:18 +0530

    First commit. Responsiveness of Home page yet to be done.

---
+++ app.js
@@ -0,0 +1,37 @@
+const port = 3000;
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+
+app.get("/", function (req, res) {
+  res.render("index")
+});
+
+app.get("/contact", function (req, res) {
+  res.render("contact");
+});
+
+app.post("/contact", function (res, req) {
+  username = req.body.fullName;
+  email = req.body.email;
+  message = req.body.message;
+
+  console.log(username);
+  console.log(email);
+  console.log(message);
+});
+
+app.get("/work", function (req, res) {
+  res.render("work");
+});
+
+
+app.listen(port, function (req, res) {
+  console.log("Server running on port 3000.");
+});
